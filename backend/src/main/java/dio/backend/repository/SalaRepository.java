20724b4d07005e577e179105237a58cb665e3f7b
Marcelo Bomfim <celodc@yahoo.com.br>
2021-09-12 10:37:18 -0300

    classe repository

---
+++ backend/src/main/java/dio/backend/repository/SalaRepository.java
@@ -0,0 +1,10 @@
+package dio.backend.repository;
+
+import dio.backend.model.Sala;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SalaRepository extends JpaRepository<Sala, Long> {
+
+}
